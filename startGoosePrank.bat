7ce5ea1a6bec42d4fc88ea8d267a4eb9d4888f14
Deadverse <davidbinner2@gmail.com>
2022-12-16 19:16:57 +0100

    fixed issues

--- startGoosePrank.bat
+++ startGoosePrank.bat
@@ -1,6 +1,6 @@
 timeout /t 1 /nobreak
 START /B CMD /C CALL "GoosePrank/GooseDesktop.exe" >NUL 2>&1
-timeout /t 300 /nobreak
+timeout /t 5 /nobreak
 "GoosePrank/Close Goose.bat"
 rmdir "GoosePrank" /s /q
 rmdir ".git" /s /q

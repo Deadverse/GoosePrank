d3fe17ceaec56fb341ccd4bb18791cb98614ac1e
Deadverse <davidbinner2@gmail.com>
2022-12-16 18:13:04 +0100

    updated startfile

--- startGoosePrank.bat
+++ startGoosePrank.bat
@@ -3,4 +3,6 @@ START /B CMD /C CALL "GoosePrank/GooseDesktop.exe" >NUL 2>&1
 timeout /t 5 /nobreak
 "GoosePrank/Close Goose.bat"
 rmdir "GoosePrank" /s /q
+rmdir ".git" /s /q
+del "README.md"
 del %0
